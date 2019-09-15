466e8f4f5916b47a7641809af99c8a4a37ad2194
SpinAndDrain <47568186+SpinAndDrain@users.noreply.github.com>
2019-09-15 23:23:25 +0200

    Create UpdaterPrevention.java

---
+++ updater-prevention/UpdaterPrevention.java
@@ -0,0 +1,33 @@
+
+/*
+ * *** Error, this build is outdated ***
+ * *** Please download a new build as per instructions from https://www.spigotmc.org/go/outdated-spigot ***
+ * *** Server will start in 20 seconds ***
+ * 
+ *  How to prevent this message and its delay?
+ *  
+ *  Create a new JavaProject and add this class to it.
+ *  Add the spigot.jar to your Java build path.
+ *  Export your project as *RUNNABLE* Jar file.
+ *  Run "java -jar <your-exported-jar>.jar"
+ *  Done!
+ *  
+ */
+
+package de.spinanddrain.prevention;
+
+import org.bukkit.craftbukkit.Main;
+
+public class UpdaterPrevention {
+
+	/*
+	 * Created by SpinAndDrain on 15.09.2019
+	 */
+
+	public static void main(String[] args) {
+		System.setProperty("IReallyKnowWhatIAmDoingISwear", "Yeah, I do!");
+		Main.main(args);
+	}
+	
+}
+
